dae8ac339b2c50c08b10de2372abd09a464089e1
Miguel MG <miguelmorgom@gmail.com>
2016-02-14 12:08:50 +0100

    Uploader listo en el archivo upload

--- view/upload.php
+++ view/upload.php
@@ -1,18 +1,26 @@
 <?php
 
-    $ubicacion = "recursos/";
+    $ubicacion = "../recursos/";
     $recurso = $ubicacion . basename($_FILES["recurso"]["name"]);
     $uploadOk = 1;
     $tipoRecurso = pathinfo($recurso, PATHINFO_EXTENSION);
 
     if(isset($_POST["btnRecurso"])) {
-        $validar = getimagesize($_FILES["recurso"]["tmp_name"]);
+        /*$validar = getimagesize($_FILES["recurso"]["tmp_name"]);
         if($validar !== false) {
             echo "File is an image - " . $validar["mime"] . ".";
             $uploadOk = 1;
         } else {
             echo "File is not an image.";
             $uploadOk = 0;
+        }*/
+
+        var_dump($_FILES["recurso"]["tmp_name"]);
+        var_dump($recurso);
+        if (move_uploaded_file($_FILES["recurso"]["tmp_name"], $recurso)){
+            echo "El archivo ha sido cargado correctamente.";
+        }else{
+            echo "Ocurrió algún error al subir el fichero. No pudo guardarse.";
         }
     }
 
